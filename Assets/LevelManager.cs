04bf9dbae4c59d093ebde097a0e03425ceb230df
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-05-09 19:14:02 +0530

    Start, Game, Win and Lose scene

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,5 +7,14 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLevel (string name)
     {
         Debug.Log("Level load requested for " + name);
+        #pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel(name);
+        #pragma warning restore CS0618 // Type or member is obsolete
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I wanna quit.");
+        Application.Quit();
     }
 }
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+    int maxGuessesAllowed = 10;
+    public Text text;
+
+    void Start () {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        max = max + 1;
+    }
+
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel("Win");
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
